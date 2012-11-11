e71cc5f34428185f05394771d3671c780d25a098
Lee Stearns <lstearns86@gmail.com>
2012-11-11 00:19:43 -0500

    initial kinect project, with people highlighting

---
+++ KinectPeopleTracker/KinectPeopleTracker/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace KinectPeopleTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayPanel = new DistanceDemos.DoubleBufferedPanel();
+            this.SuspendLayout();
+            // 
+            // DisplayPanel
+            // 
+            this.DisplayPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DisplayPanel.Location = new System.Drawing.Point(0, 0);
+            this.DisplayPanel.Name = "DisplayPanel";
+            this.DisplayPanel.Size = new System.Drawing.Size(640, 480);
+            this.DisplayPanel.TabIndex = 0;
+            this.DisplayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DisplayPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 480);
+            this.Controls.Add(this.DisplayPanel);
+            this.Name = "Form1";
+            this.Text = "Kinect People Tracker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DistanceDemos.DoubleBufferedPanel DisplayPanel;
+    }
+}
+
---
+++ KinectPeopleTracker/KinectPeopleTracker/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Kinect;
+using System.Drawing.Imaging;
+
+namespace KinectPeopleTracker
+{
+    public partial class Form1 : Form
+    {
+        private KinectSensor sensor;
+        private Color[] playerColors = { Color.White, Color.Blue, Color.Red, Color.Green, Color.Yellow, Color.Cyan, Color.Magenta };
+        private Bitmap depthImage = null;
+        private DepthImagePixel[] depthData;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            sensor = KinectSensor.KinectSensors[0];
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            sensor.SkeletonStream.Enable();
+            sensor.DepthFrameReady += new EventHandler<DepthImageFrameReadyEventArgs>(sensor_DepthFrameReady);
+            depthData = new DepthImagePixel[sensor.DepthStream.FramePixelDataLength];
+            sensor.Start();
+        }
+
+        void sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            try
+            {
+                DepthImageFrame frame = e.OpenDepthImageFrame();
+
+                if (frame != null)
+                {
+                    frame.CopyDepthImagePixelDataTo(depthData);
+                    int maxDepth = frame.MaxDepth;
+                    int minDepth = frame.MinDepth;
+                    lock (this)
+                    {
+                        if (depthImage == null) depthImage = new Bitmap(frame.Width, frame.Height);
+
+                        // write over the existing image pixel by pixel
+                        BitmapData data = depthImage.LockBits(new Rectangle(0, 0, depthImage.Width, depthImage.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+                        unsafe
+                        {
+                            byte* p = (byte*)data.Scan0;
+                            int i = 0;
+                            for (int y = 0; y < data.Height; y++)
+                            {
+                                for (int x = 0; x < data.Width; x++)
+                                {
+                                    int depth = depthData[i].Depth;
+                                    if (!depthData[i].IsKnownDepth) depth = maxDepth;
+                                    int player = depthData[i].PlayerIndex;
+                                    Color c = playerColors[player];
+                                    float depthPercent = 1.0f - (float)(depth - minDepth) / (float)(maxDepth - minDepth);
+                                    *p = (byte)(c.R * depthPercent);
+                                    *(p + 1) = (byte)(c.G * depthPercent);
+                                    *(p + 2) = (byte)(c.B * depthPercent);
+                                    p += 3;
+                                    i++;
+                                }
+                                p += data.Stride - 3 * data.Width;
+                            }
+                        }
+                        depthImage.UnlockBits(data);
+
+                        Invoke(new MethodInvoker(delegate { DisplayPanel.Refresh(); }));
+                    }
+
+                    frame.Dispose();
+                }
+            }
+            catch { }
+        }
+
+        private void DisplayPanel_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                if (depthImage != null)
+                {
+                    lock (this)
+                    {
+                        e.Graphics.DrawImage(depthImage, 0, 0, depthImage.Width, depthImage.Height);
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sensor != null) sensor.Stop();
+        }
+    }
+}
